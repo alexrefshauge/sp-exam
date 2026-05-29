73796f25217478d9ee01915af12e12f1ceceeb8e
alexrefshauge <nk02hb@aau.student.dk>
2026-05-29 16:00:51 +0200

    before test option

--- lib/tests/test_pool.cpp
+++ lib/tests/test_pool.cpp
@@ -1,10 +1,13 @@
 #include <doctest/doctest.h>
 #include "Sim.hpp"
+#include <iostream>
 
 TEST_SUITE("Sim pool tests")
 {
     TEST_CASE("Sim pool joins and terminates")
     {
-        stochastic::SimPool{};
+        stochastic::SimPool pool{};
+        pool.repeatJob(100, [&](int i)
+                       { std::cout << "done " << i << std::endl; });
     }
 }
